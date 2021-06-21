6cb84c4ef0919f0bd9ba95aff365af09cd78b53e
Nikolay Edigaryev <edigaryev@gmail.com>
2021-06-21 14:27:07 +0300

    TerminalHost: provide missing methods on Windows

--- pkg/host/host_windows.go
+++ pkg/host/host_windows.go
@@ -1,7 +1,21 @@
 package host
 
-import "fmt"
+import (
+	"context"
+	"errors"
+	"time"
+)
+
+var ErrUnsupported = errors.New("Cirrus Terminal doesn't support Windows yet, see https://github.com/creack/pty/pull/109")
 
 func New(opts ...Option) (*TerminalHost, error) {
-	return nil, fmt.Errorf("Cirrus Terminal doesn't support Windows yet, see https://github.com/creack/pty/pull/109")
+	return nil, ErrUnsupported
+}
+
+func (th *TerminalHost) Run(ctx context.Context) error {
+	return ErrUnsupported
+}
+
+func (th *TerminalHost) LastActivity() time.Time {
+	return time.Time{}
 }
